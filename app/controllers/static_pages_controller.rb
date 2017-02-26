bc0367b1b9e4739d99e881bf71bac4762f2e3cad
Beau Carnes <beaucarnes@gmail.com>
2017-02-26 11:48:27 +0000

    additional layout wording changes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
-    @posts_rp = Post.where(category: Post.categories[:request_prayer], status: Post.statuses[:active]).take(4)
-    @posts_rh = Post.where(category: Post.categories[:request_help], status: Post.statuses[:active]).take(4)
-    @posts_op = Post.where(category: Post.categories[:offer_prayer], status: Post.statuses[:active]).take(4)
-    @posts_oh = Post.where(category: Post.categories[:offer_help], status: Post.statuses[:active]).take(4)
-    @posts_met = Post.where(status: Post.statuses[:met]).take(2)
+    @posts = Post.where(status: Post.statuses[:active]).take(6)
+    @posts_met = Post.where(status: Post.statuses[:met]).take(4)
   end
 
   def help
